0702bfd4b78825ff9fc132431a2c34da61ea9c30
MSI\wanko <wankomidori9948@gmail.com>
2023-04-18 12:40:57 +0900

    シーン遷移大体終わり

--- Assets/Script/Sugisaki/SceneChange.cs
+++ Assets/Script/Sugisaki/SceneChange.cs
@@ -6,39 +6,63 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] string sceneName = "Test1";
+    [SerializeField] GameObject cameraObject;
 
-    [SerializeField] GameObject trasnsitionObject;
+    [SerializeField] GameObject FadeInObject;
+    [SerializeField] GameObject FadeOutObject;
 
-    readonly float waitTime = 0.5f;
+    readonly float waitTime = 1.0f;
+
+    public float stopTime;
 
     public bool isFade;
+
+    private float time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        isFade = false;
+        isFade = true;
+
+        Vector3 position = cameraObject.transform.position;
+
+        position.z = position.z + 1.0f;
+
+        Instantiate(FadeOutObject, position, cameraObject.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        time += Time.deltaTime;
 
-    private void FixedUpdate()
-    {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(time>waitTime)
+        {
+            isFade = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             StartCoroutine(nameof(LoadScene));
+
+            time = 0.0f;
         }
     }
 
+
     IEnumerator LoadScene()
     {
-        Instantiate(trasnsitionObject);
+        isFade = true;
+
+        Vector3 position = cameraObject.transform.position;
+
+        position.z = position.z + 1.0f;
+
+        Instantiate(FadeInObject, position, cameraObject.transform.rotation);
 
         yield return new WaitForSeconds(waitTime);
 
-        SceneManager.LoadScene(sceneName);
+        if (time > stopTime)
+            SceneManager.LoadScene(sceneName);
     }
 }
