91e0694a016608cc42235925c996b7eea3462a6f
MSI\clclm <hal88k5@gmail.com>
2023-04-29 19:26:21 +0900

    シャッター実装したり

--- Assets/Script/Shimokawara/CookMoveManager.cs
+++ Assets/Script/Shimokawara/CookMoveManager.cs
@@ -6,24 +6,60 @@ public class CookMoveManager : MonoBehaviour
 {
 
     public GameObject[] GameStage;//最初は本作業場
+    public int FadesTime = 20;
+
+    public int FPS_Time = 0;
+
+    static int SYATTA_TIME = 30;
+
+    public GameObject Syatta;
+    float SyattaMax;
+    float SyattaMin = 0.873f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SyattaMax = Syatta.transform.localPosition.y;
+        FPS_Time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+#if UNITY_EDITOR
         if(Input.GetButtonDown("Jump"))
         {
-            NextScene();
-            Debug.Log("呼ばれた");
+            FPS_Time = FadesTime * 60 - SYATTA_TIME;
+            //NextScene();
+            //Debug.Log("呼ばれた");
         }
+#endif
     }
 
+    private void FixedUpdate()
+    {
+        FPS_Time++;
 
+        if(FPS_Time > FadesTime * 60)
+        {
+            NextScene();
+            FPS_Time = 0;
+
+            Syatta.transform.localPosition = new Vector3(Syatta.transform.localPosition.x, SyattaMin, Syatta.transform.localPosition.z);
+        }
+        //前後Nフレームで移動アニメーション
+        else if(Mathf.Abs(( FadesTime * 60 / 2) - FPS_Time)  > (FadesTime * 60 / 2) - SYATTA_TIME)
+        {
+            float PosY = (float)((FadesTime * 60 / 2) - Mathf.Abs((FadesTime * 60 / 2) - FPS_Time)) / SYATTA_TIME * (SyattaMax - SyattaMin) + SyattaMin;
+            Syatta.transform.localPosition = new Vector3(Syatta.transform.localPosition.x, PosY, Syatta.transform.localPosition.z);
+        }
+
+        else
+        {
+            Syatta.transform.localPosition = new Vector3(Syatta.transform.localPosition.x, SyattaMax, Syatta.transform.localPosition.z);
+        }
+    }
 
     public void NextScene()
     {
