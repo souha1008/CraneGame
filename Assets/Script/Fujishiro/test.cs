87b2afefef1521367529e27683e3f3b66c637453
Fujishiro Haruki <fujishiro.haruki.hal@gmail.com>
2023-04-29 09:47:26 +0900

    testシーン編集

--- Assets/Script/Fujishiro/test.cs
+++ Assets/Script/Fujishiro/test.cs
@@ -13,6 +13,9 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SuccessManager.instance.Success_UIActive();
+        }
     }
 }
