ff2a27395539cf2e6d6471e649d3d4b081eeff05
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2023-04-30 21:00:30 +0900

    メダルゲームとちゅ

--- Assets/Script/Fujishiro/MiniGame/Hopper.cs
+++ Assets/Script/Fujishiro/MiniGame/Hopper.cs
@@ -7,12 +7,12 @@ public class Hopper : MonoBehaviour
     [SerializeField] GameObject Prefab_Coin;
     [SerializeField] float speed;
 
-    
+    Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = this.transform.position;
     }
 
     // Update is called once per frame
@@ -26,6 +26,13 @@ public class Hopper : MonoBehaviour
         {
             var coin = Instantiate(Prefab_Coin);
             Vector3 force = Vector3.forward * speed;
+            coin.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            var coin = Instantiate(Prefab_Coin, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+            Vector3 force = Vector3.up * speed * 100;
             coin.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
         }
     }
