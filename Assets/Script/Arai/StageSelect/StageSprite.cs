3bad483fcfe65ca7e7bfc08be0ea7cb5f6b6cc7b
Iris_AI <io.jupiter1.1.1@gmail.com>
2023-05-09 19:09:57 +0900

    再・メーター改修＆セレクト画面仮設

--- Assets/Script/Arai/StageSelect/StageSprite.cs
+++ Assets/Script/Arai/StageSelect/StageSprite.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageSprite : Select_UI
+using UnityEngine.UI;
+
+public class StageSprite : MonoBehaviour
 {
+    private Image image;
+
+    void Awake()
+    {
+        image = gameObject.GetComponent<Image>();
+    }
+
+    public void Activate()
+    {
+        image.color = Color.red;
+    }
     
+    public void Inactivate()
+    {
+        image.color = Color.blue;
+    }
 }
