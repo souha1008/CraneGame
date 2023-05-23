41f165896826dea79a9909493e1eed9e2133dcfb
Fujishiro Haruki <fujishiro.haruki.hal@gmail.com>
2023-05-23 11:31:21 +0900

    サウンドマネージャーカッコカリ

--- Assets/Script/Fujishiro/SoundManager.cs
+++ Assets/Script/Fujishiro/SoundManager.cs
@@ -3,66 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct SOUND_STRUCT
+struct SOUND_STRUCT
 {
-    [SerializeField] string name;
-    [SerializeField] AudioClip audioclip;
-};
+    public string name;
+    public AudioClip audioClip;
+}
+
 
 public class SoundManager : MonoBehaviour
 {
-    public SoundManager instance;
+    public static SoundManager instance;
     [SerializeField] AudioSource audioSource;
 
-    [SerializeField] SOUND_STRUCT[] bgm_s;
+    [SerializeField] SOUND_STRUCT[] keyBGM;
+    [SerializeField] SOUND_STRUCT[] keySE;
 
-    [SerializeField] AudioClip[] BGM_Clip;
-    [SerializeField] AudioClip[] SE_Clip;
 
-    [SerializeField] static AudioClip[] audioClips;
-
-    [SerializeField]
-    Dictionary<string, int> keyBGM = new Dictionary<string, int>
-    {
-        {"タイトルBGM", 0},
-        {"ステージセレクト", 1},
-        {"リザルトBGM", 2 },
-        {"ゲーム本編BGM_1", 3 },
-        {"ゲーム本編BGM_2", 4 },
-        {"ゲーム本編BGM_3", 5 },
-        {"ゲーム本編BGM_4", 6 },
-    };
-
-    Dictionary<string, int> keySE = new Dictionary<string, int>
-    {
-        {"プレスエニーSE", 0},
-        {"決定SE", 1},
-        {"選択SE", 2 },
-        {"音量調整SE", 3 },
-        {"戻るSE", 4 },
-        {"ステージ決定SE", 5 },
-        {"エンディングSE", 6 },
-        {"ポーズ開くSE", 7 },
-        {"ポーズ閉じるSE", 8 },
-        {"アーム前後左右移動SE", 9 },
-        {"アーム上下移動SE", 10 },
-        {"アタッチメント変更SE", 11 },
-        {"包丁射出SE", 12 },
-        {"ハンマー空振りSE", 13 },
-        {"バーナー噴射SE", 14 },
-        {"おもちゃ潰しSE", 15 },
-        {"卵落下SE", 16 },
-        {"ハンマー空振りSE", 17 },
-        {"バーナー噴射SE", 18 },
-        {"SE", 20 },
-    };
-
-    
-    
     // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+            if (instance != null)
+            {
+                Debug.Log("入れたよ");
+            }
+        }
     }
 
     // Update is called once per frame
@@ -72,21 +39,39 @@ public class SoundManager : MonoBehaviour
     }
 
     public void BGMPlay(string playTitle)
-    {
-        audioSource.clip = BGM_Clip[keyBGM[playTitle]];
-
+    {   
+        for(int i = 0; i < keyBGM.Length; i++)
+        {
+            if(playTitle == keyBGM[i].name)
+            {
+                audioSource.clip = keyBGM[i].audioClip;
+            }
+        }
+        
         audioSource.Play();
     }
     public void BGMPlay(string playTitle, bool isloop)
     {
-        audioSource.clip = BGM_Clip[keyBGM[playTitle]];
+        for (int i = 0; i < keyBGM.Length; i++)
+        {
+            if (playTitle == keyBGM[i].name)
+            {
+                audioSource.clip = keyBGM[i].audioClip;
+            }
+        }
         audioSource.loop = isloop;
 
         audioSource.Play();
     }
     public void BGMPlay(string playTitle, bool isloop, float volume)
     {
-        audioSource.clip = BGM_Clip[keyBGM[playTitle]];
+        for (int i = 0; i < keyBGM.Length; i++)
+        {
+            if (playTitle == keyBGM[i].name)
+            {
+                audioSource.clip = keyBGM[i].audioClip;
+            }
+        }
         audioSource.loop = isloop;
         audioSource.volume = volume;
 
@@ -95,6 +80,24 @@ public class SoundManager : MonoBehaviour
 
     public void SEPlay(string playTitle)
     {
-        audioSource.PlayOneShot(SE_Clip[keySE[playTitle]]);
+        for(int i = 0; i < keySE.Length; i++)
+        {
+            if (playTitle == keySE[i].name)
+            {
+                audioSource.PlayOneShot(keySE[i].audioClip);
+            }
+        }
+    }
+
+    public void SEPlay(string playTitle, float volume)
+    {
+        audioSource.volume = volume;
+        for (int i = 0; i < keySE.Length; i++)
+        {
+            if (playTitle == keySE[i].name)
+            {
+                audioSource.PlayOneShot(keySE[i].audioClip);
+            }
+        }
     }
 }
