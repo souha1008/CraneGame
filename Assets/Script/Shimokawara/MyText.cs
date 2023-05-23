6353926f4cf153989235539d27969f0affa6b0ed
MSI\wanko <wankomidori9948@gmail.com>
2023-05-23 11:46:26 +0900

    タイマー

--- Assets/Script/Shimokawara/MyText.cs
+++ Assets/Script/Shimokawara/MyText.cs
@@ -6,17 +6,37 @@ using TMPro; //TextMeshProを扱う際に必要
 public class MyText : MonoBehaviour
 {
     TextMeshProUGUI text;
-    int a = 0;
+
+    int speedClearTime; // タイムリミット秒
+    int FPSTime;    // 今フェーズのフレーム数
+    int AllTime;    // それまでのフレーム数
+
+    float OneFrame; // 1フレームの秒数 
+
+    int time = 0;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
         text.text = "9999";
+
+        speedClearTime = CookMoveManager.instance.SPEED_CLEAR_TIME;
+        FPSTime = CookMoveManager.instance.FPS_Time;
+        AllTime = CookMoveManager.instance.AllTime;
+
+        OneFrame = 1.0f / 60.0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //GetComponent<TextMeshPro>().text = "999";
+        FPSTime = CookMoveManager.instance.FPS_Time;
+        AllTime = CookMoveManager.instance.AllTime;
+        
+        int FrameTime = FPSTime + AllTime;
+        float SecTime = (float)FrameTime * OneFrame;
+        time = speedClearTime - (int)SecTime;
+
+        text.text = time.ToString("000");
     }
 }
