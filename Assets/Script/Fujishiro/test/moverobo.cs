9af3612bbc1159be2c716b20b8e563c8bc28e7e1
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2023-05-24 10:37:08 +0900

    チュートリアル表示のや～つできた

--- Assets/Script/Fujishiro/test/moverobo.cs
+++ Assets/Script/Fujishiro/test/moverobo.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kogasitest : MonoBehaviour
+public class moverobo : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        Material_ColorTransfer.instance.Scorch_Object(1.4f);
+        this.transform.position += new Vector3(0.1f, 0, 0);
     }
 }
