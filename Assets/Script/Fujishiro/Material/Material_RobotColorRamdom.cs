dc74c5678673cc8792c0100410b461c498f7c12a
Fujishiro Haruki <fujishiro.haruki.hal@gmail.com>
2023-05-27 13:47:07 +0900

    Luminanceをランダムに変更できるようにしました

--- Assets/Script/Fujishiro/Material/Material_RobotColorRamdom.cs
+++ Assets/Script/Fujishiro/Material/Material_RobotColorRamdom.cs
@@ -13,16 +13,29 @@ public class Material_RobotColorRamdom : MonoBehaviour
         rand_ = Random.Range(0.0f, 1.0f);
 
         skinnedmeshRenderer.material.SetFloat("_Hue", rand_);
+
+        skinnedmeshRenderer.material.SetVector("_Luminance",
+    new Vector4(Random.Range(-0.25f, 0),
+    Random.Range(-0.25f, 0),
+    Random.Range(-0.25f, 0), 0));
     }
 
     // Update is called once per frame
     void Update()
     {
+#if UNITY_EDITOR
         if(Input.GetKeyDown(KeyCode.Space))
         {
             rand_ = Random.Range(0.0f, 1.0f);
 
             skinnedmeshRenderer.material.SetFloat("_Hue", rand_);
+
+
+            skinnedmeshRenderer.material.SetVector("_Luminance",
+                new Vector4(Random.Range(-0.25f, 0),
+                Random.Range(-0.25f, 0),
+                Random.Range(-0.25f, 0), 0));
         }
+#endif
     }
 }
