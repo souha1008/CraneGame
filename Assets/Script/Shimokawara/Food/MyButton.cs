f85f5586bca6092fecb40c5945a6b2dc83c2e29b
MSI\clclm <hal88k5@gmail.com>
2023-05-28 09:20:30 +0900

    2-4＆ボタン調整

--- Assets/Script/Shimokawara/Food/MyButton.cs
+++ Assets/Script/Shimokawara/Food/MyButton.cs
@@ -48,25 +48,33 @@ public class MyButton: CircleFoodsInterFace
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "AttachFire")
-            ButtonState = true;
-        if (other.gameObject.tag == "AttachKnife")
-            ButtonState = true;
-        if (other.gameObject.tag == "AttachHummer")
-            ButtonState = true;
-        if (other.gameObject.tag == "LeftArm")
-            ButtonState = true;
-        if (other.gameObject.tag == "RightArm")
-            ButtonState = true;
+        if (Cnt > 120)
+        {
+            if (other.gameObject.tag == "AttachFire")
+                ButtonState = true;
+            if (other.gameObject.tag == "AttachKnife")
+                ButtonState = true;
+            if (other.gameObject.tag == "AttachHummer")
+                ButtonState = true;
+            if (other.gameObject.tag == "LeftArm")
+                ButtonState = true;
+            if (other.gameObject.tag == "RightArm")
+                ButtonState = true;
+        }
+
+        
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "LeftArm")
-            ButtonState = true;
-        if (collision.gameObject.tag == "RightArm")
-            ButtonState = true;
+        if (Cnt > 120)
+        {
+            if (collision.gameObject.tag == "LeftArm")
+                ButtonState = true;
+            if (collision.gameObject.tag == "RightArm")
+                ButtonState = true;
+        }
     }
 
     public void ButtonReset()
