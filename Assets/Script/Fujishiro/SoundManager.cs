1c8fcdcd7f772609a23969aefdbf2628b20015c4
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2023-05-30 21:57:27 +0900

    BGMStopを実行した際、clipを破棄するように

--- Assets/Script/Fujishiro/SoundManager.cs
+++ Assets/Script/Fujishiro/SoundManager.cs
@@ -146,6 +146,7 @@ public class SoundManager : MonoBehaviour
     public void BGMStop()
     {
         BGMaudioSource.Stop();
+        BGMaudioSource.clip = null;
     }
 
     public void SEStop()
