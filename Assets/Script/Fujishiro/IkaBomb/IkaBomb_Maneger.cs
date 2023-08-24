77d346e22d3d83ae824d3ea27fa137302466a280
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2023-08-24 14:41:59 +0900

    イカ墨爆弾ギミック

---
+++ Assets/Script/Fujishiro/IkaBomb/IkaBomb_Maneger.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class IkaBomb_Maneger : MonoBehaviour
+{
+    static public IkaBomb_Maneger instance;
+
+    [SerializeField] SpriteRenderer[] Sprite_sumi;
+
+    [SerializeField] float alpha_value;
+    [SerializeField] bool UseSumi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        for (int i = 0; i < Sprite_sumi.Length; i++)
+        {
+            Sprite_sumi[i].color = new Color(0, 0, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 徐々に墨が見えなくなる
+        if (UseSumi)
+        {
+            for (int i = 0; i < Sprite_sumi.Length; i++)
+            {
+                var color = Sprite_sumi[i].color;
+                color.a -= alpha_value / 10000;
+                Sprite_sumi[i].color = color;
+            }
+            if (Sprite_sumi[0].color.a < 0)
+            {
+                UseSumi = false;
+            }
+        }
+    }
+
+    public void Bomb(Collider other)
+    {
+        if (other.gameObject.tag == "AttachKnife")
+        {
+            UseSumi = true;
+            // ナイフが当たった処理
+            for (int i = 0; i < Sprite_sumi.Length; i++)
+            {
+                Sprite_sumi[i].color = new Color(1, 1, 1, 5);
+
+            }
+
+        }
+    }
+}
