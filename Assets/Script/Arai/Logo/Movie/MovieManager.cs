5cbaef69cefc277eb1c134cdf386e5e8be0d69e1
Iris_AI <io.jupiter1.1.1@gmail.com>
2023-09-24 10:12:34 +0900

    Add MovieManager

--- Assets/Script/Arai/Logo/Movie/MovieManager.cs
+++ Assets/Script/Arai/Logo/Movie/MovieManager.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovieManager : MonoBehaviour
 {
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private RectTransform image;
+
+    [SerializeField]
+    private int waittime = 10;
+
+    bool wait = false;
+
     void Start()
     {
         
@@ -13,7 +25,29 @@ public class MovieManager : MonoBehaviour
     {
         if (Input.GetButton("Debug Multiplier"))
         {
-            GameObject.Find("SceneChange").GetComponent<SceneChange>().LoadScene("TitleTest");
+            if(wait) StopCoroutine(Wait());
+            SceneChange();
         }
+        else if (image.anchoredPosition.y <= 0)
+        {
+            wait = true;
+            StartCoroutine(Wait());
+        }
+    }
+
+    private void SceneChange()
+    {
+        camera.gameObject.name = new string("Main Camera");
+        GameObject.Find("SceneChange").GetComponent<SceneChange>().LoadScene("TitleTest");
+        this.enabled = false;
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(waittime);
+
+        SceneChange();
+
+        yield break;
     }
 }
