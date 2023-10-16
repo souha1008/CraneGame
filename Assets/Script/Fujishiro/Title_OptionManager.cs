2df4f0088ea1fcc0ce83bd515f5503bd08a89815
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2023-10-16 14:59:13 +0900

    ちょいバグ編集

--- Assets/Script/Fujishiro/Title_OptionManager.cs
+++ Assets/Script/Fujishiro/Title_OptionManager.cs
@@ -8,8 +8,7 @@ public class Title_OptionManager : MonoBehaviour
     public static Title_OptionManager instance;
     [SerializeField][Tooltip("�������猈�肷��")] KeyCode KetteiKey = KeyCode.Joystick1Button1;
     [SerializeField][Tooltip("��������L�����Z��")] KeyCode BackKey = KeyCode.Joystick1Button0;
-    [SerializeField][Tooltip("���L�[")] KeyCode DownArrow = KeyCode.DownArrow;
-    [SerializeField][Tooltip("���L�[")] KeyCode UpArrow = KeyCode.UpArrow;
+    [SerializeField][Tooltip("���L�[")] string DPAD_hori = "JuujiKeyY";
 
     [SerializeField] GameObject Option_Canvas;
     [SerializeField] Slider BGM_slider;
@@ -31,6 +30,7 @@ public class Title_OptionManager : MonoBehaviour
 
 
     private float prevAxis = 0;
+    private float DPADAxis = 0;
     private float slider_nowcoolframe;
 
     // Start is called before the first frame update
@@ -83,19 +83,21 @@ public class Title_OptionManager : MonoBehaviour
 
         while(true)
         {
-            if (Input.GetKeyDown(UpArrow) || (Input.GetAxis("Vertical") > 0.3f && prevAxis == 0))
+            if ((Input.GetAxis(DPAD_hori) > 0.8f && DPADAxis == 0) || (Input.GetAxis("Vertical") > 0.3f && prevAxis == 0))
             {
                 OptionSelectCount--;
                 prevAxis = Input.GetAxis("Vertical");
+                DPADAxis = Input.GetAxis(DPAD_hori);
                 if (GameObject.Find("SoundManager"))
                     SoundManager.instance.SEPlay("選択SE");
                 if (OptionSelectCount < 0) OptionSelectCount = 1;
             }
 
-            if (Input.GetKeyDown(DownArrow) || (Input.GetAxis("Vertical") < -0.3f && prevAxis == 0))
+            if ((Input.GetAxis(DPAD_hori) < -0.8f && DPADAxis == 0) || (Input.GetAxis("Vertical") < -0.3f && prevAxis == 0))
             {
                 OptionSelectCount++;
                 prevAxis = Input.GetAxis("Vertical");
+                DPADAxis = Input.GetAxis(DPAD_hori);
                 if (GameObject.Find("SoundManager"))
                     SoundManager.instance.SEPlay("選択SE");
                 if (OptionSelectCount > 1) OptionSelectCount = 0;
@@ -106,9 +108,13 @@ public class Title_OptionManager : MonoBehaviour
             {
                 prevAxis = 0;
             }
+            if(Input.GetAxis(DPAD_hori) == 0)
+            {
+                DPADAxis = 0;
+            }
 
             // �I�v�V�����I���
-            if(Input.GetKeyDown(BackKey))
+            if (Input.GetKeyDown(BackKey))
             {
                 Option_Canvas.SetActive(false);
                 isOption = false;
